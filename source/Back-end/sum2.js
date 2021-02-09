dee36f4162be8927f8d53acc8aa9b24bd883f98e
Kevin Lee <sukchan.kevin@gmail.com>
2021-02-09 12:29:02 -0800

    Added JSDoc Commentary

--- source/Back-end/sum2.js
+++ source/Back-end/sum2.js
@@ -1,3 +1,9 @@
+/** 
+ * Adds two numbers (duh)
+ * @param {int} a - first number you want to add.
+ * @param {int} b - second number you want to add.
+ */
+
 function sum2(a, b) {
   return a + b;
 }
