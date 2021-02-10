76182b96b873642c245142b53be1712c334580c8
Kevin Lee <sukchan.kevin@gmail.com>
2021-02-09 22:31:44 -0800

    Update sum2.js

--- source/Back-end/sum2.js
+++ source/Back-end/sum2.js
@@ -1,7 +1,7 @@
 /** 
  * Adds two numbers (duh, really?)
  * @param {int} a - first number you want to add.
- * @param {int} b - second number you want to add.
+ * @param {int} b - second number you want to add
  */
 
 function sum2(a, b) {
