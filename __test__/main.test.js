2cc71120cde79436c31f95c307473b10db6471b9
ethanh6 <38364796+ethanh6@users.noreply.github.com>
2021-02-23 22:07:11 +0800

    add fillColor test

--- __test__/main.test.js
+++ __test__/main.test.js
@@ -2,11 +2,10 @@
  * @jest-environment jsdom
  */
 
-const { getByText, getByTestId, fireEvent } = require("@testing-library/dom");
+// import 'jest-dom/extend-expect'
+// import { render, cleanup } from 'react-testing-library'
 
-describe("timeToSec", () => {
-
-  document.body.innerHTML = `
+document.body.innerHTML = `
     <div id="container">
         <header id="header">
             <h1>PomoTime</h1>
@@ -81,25 +80,53 @@ describe("timeToSec", () => {
 
         </footer>
     </div>
-  `;
+`
 
-//   load timeToSec from main.js
-  const modules = require("../source/Front-end/javascript/main.js");
-  const timeToSec = modules.timeToSec;
+const { getByText, getByTestId, fireEvent } = require("@testing-library/dom");
+const { timeToSec, secToTime, fillColor } = require("../source/Front-end/javascript/main.js");
 
-  test("should turn 02:00 into 120", () => {
-    expect(timeToSec("02:00")).toBe(120);
-  });
+describe("timeToSec testing", () => {
 
-  test("should turn 00:00 into 0", () => {
-    expect(timeToSec("00:00")).toBe(0);
-  });
+    test("should turn 02:00 into 120", () => {
+        expect(timeToSec("02:00")).toBe(120);
+    });
 
-  test("should turn 05:22 into 322", () => {
-    expect(timeToSec("05:22")).toBe(322);
-  });
+    test("should turn 00:00 into 0", () => {
+        expect(timeToSec("00:00")).toBe(0);
+    });
 
-//   test("should set max of input", () => {
-//     expect(timeToSec("99:99")).toBe(23*60+59);
-//   });
+    test("should turn 05:22 into 322", () => {
+        expect(timeToSec("05:22")).toBe(322);
+    });
+
+    //   test("should set max of input", () => {
+    //     expect(timeToSec("99:99")).toBe(23*60+59);
+    //   });
+
+});
+
+describe("setToTime testing", () => {
+
+    test("should turn 120 into 02:00", () => {
+        expect(secToTime(120)).toBe("02:00");
+    });
+
+    test("should turn 0 into 00:00", () => {
+        expect(secToTime(0)).toBe("00:00");
+    });
+
+    test("should turn 322 into 05:22", () => {
+        expect(secToTime(322)).toBe("05:22");
+    });
+
+});
+
+describe("fillColor testing", () => {
+    test("should render style", () => {
+        fillColor();
+        expect(document.getElementById("header")).toHaveStyle(`
+            backgroundColor = "lightgreen",
+            color = "white"
+        `);
+    });
 });
