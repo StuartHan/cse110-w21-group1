73f30a7ba2d8dfa7376c2f1e134b99707c5585f9
EthanHuang <ethuang@ucsd.edu>
2021-02-28 03:15:14 +0800

    add runCounter, TOOD: autoSwitchMOde

--- __test__/main.test.js
+++ __test__/main.test.js
@@ -83,7 +83,7 @@ document.body.innerHTML = `
 `;
 
 const { getByText, getByTestId, fireEvent } = require("@testing-library/dom");
-const {
+let {
   // methods
   timeToSec,
   secToTime,
@@ -93,11 +93,14 @@ const {
   countDown,
   autoSwitchMode,
   changeMode,
+  runCounter,
 
   // vars
   workSec,
   totalSec,
   currMode,
+  currSec,
+  counts,
 } = require("../source/Front-end/javascript/main.js");
 
 describe("timeToSec testing", () => {
@@ -245,28 +248,9 @@ describe("updateTable testing", () => {
   });
 });
 
-describe("countDown testing", () => {
-  test("totalSec = 0, should switch mode", () => {
-    let totalSec = 0;
-    countDown();
-
-    // TODO
-    expect(document.getElementById("sound-effect").played.length).toBe(0);
-  });
-});
-
-describe("autoSwitchMode testing", () => {
-  // TODO
-  test("test", () => {
-    expect(1).toBe(1);
-  });
-});
-
 describe("changeMode testing", () => {
-  // TODO
-
   test("change HTML content when radio-working-mode is checked", () => {
-document.body.innerHTML = `
+    document.body.innerHTML = `
     <div id="container">
         <header id="header">
             <h1>PomoTime</h1>
@@ -347,7 +331,7 @@ document.body.innerHTML = `
   });
 
   test("change HTML content when radio-shortBreak-mode is checked", () => {
-document.body.innerHTML = `
+    document.body.innerHTML = `
     <div id="container">
         <header id="header">
             <h1>PomoTime</h1>
@@ -427,7 +411,7 @@ document.body.innerHTML = `
     expect(document.getElementById("time").innerHTML).toBe(secToTime(workSec));
   });
   test("change HTML content when radio-longBreak-mode is checked", () => {
-document.body.innerHTML = `
+    document.body.innerHTML = `
     <div id="container">
         <header id="header">
             <h1>PomoTime</h1>
@@ -507,3 +491,125 @@ document.body.innerHTML = `
     expect(document.getElementById("time").innerHTML).toBe(secToTime(workSec));
   });
 });
+
+describe("countDown testing", () => {
+  test("totalSec = 0, should call autoSwitchMode", () => {
+    expect(1).toBe(1);
+    // ------ TODO: remaining test case: when totalSec is 0, autoSwitch would be called once
+    //     let mockfunc = jest.fn();
+    //     autoSwitchMode = mockfunc;
+
+    //     totalSec = 0;
+    //     countDown();
+
+    //     expect(document.getElementById("sound-effect").paused).toBe(true);
+    //     expect(autoSwitchMode).toHaveBeenCalled();
+  });
+
+  test("totalSec = 2, should update HTML content to 00:01", () => {
+    totalSec = 2;
+    countDown();
+
+    expect(document.getElementById("time").innerHTML).toBe("00:01");
+  });
+});
+
+// TODO
+describe("autoSwitchMode testing", () => {
+  test("currMode = w, counts != 4 --> enter short break mode", () => {
+    document.body.innerHTML = `
+    <div id="container">
+        <header id="header">
+            <h1>PomoTime</h1>
+            <h2 id="welcome">Welcome Guest!</h2>
+            <div id="options">
+                <!--<h2>Options</h2> -->
+                <!--<h2 id="stats">Stats</h2> To be implemented later.-->
+                <img id="gear" src="./source/Front-end/css/assets/Geartransparent.png">
+            </div>
+        </header>
+ 
+        <main>
+            <div id="clock">
+                <h1 id="time">25:00</h1>
+            </div>
+ 
+            <fieldset id="mode-selection">
+                <div id="radio-working-mode-container">
+                    <input type="radio" id="radio-working-mode" name="radio-mode" checked>
+                    <label for="radio-working-mode">Work</label>
+                </div>
+                <div id="radio-shortBreak-mode-container">
+                    <input type="radio" id="radio-shortBreak-mode" name="radio-mode">
+                    <label for="radio-shortBreak-mode">Short Break</label>
+                </div>
+                <div id="radio-longBreak-mode-container">
+                    <input type="radio" id="radio-longBreak-mode" name="radio-mode">
+                    <label for="radio-longBreak-mode">Long Break</label>
+                </div>
+            </fieldset>
+
+            <table id="chart">
+                <tr id="workPhase">
+                    <td>Work Phase</td>
+                    <td>25m</td>
+                </tr>
+                <tr>
+                    <td>&#8597</td>
+                    <td id="counter">4x</td>
+                </tr>
+                <tr id="shortBreak">
+                    <td>Short Break</td>
+                    <td>5m</td>
+                </tr>
+                <tr>
+                    <td>&#8595</td>
+                </tr>
+                <tr id="longBreak">
+                    <td>Long Break</td>
+                    <td>15m</td>
+                </tr>
+            </table>
+
+            <div id="start-btn-container">
+                <button id="start-btn" type="button">START</button>
+            </div>
+
+            <audio id="sound-effect" src="./source/Front-end/css/assets/bellChime.mp3"></audio>
+        </main>
+
+        <span id="settingsMenu">
+            <h2 id="exitSettings">X</h2>
+            <h2 id="settingsTitle">Settings</h2>
+            <div id="volume-controls">
+                <img id="volume-pic" src="source/Front-end/css/assets/volume-level-3.svg">
+                <input id="volume-slider" type="range" min="0" max="100" value="100">
+            </div>
+            <button id="statistics">Statistics</button>
+        </span>
+
+        <footer id="footer">
+
+        </footer>
+    </div>
+`;
+    currMode = "w";
+    counts = 4;
+
+    autoSwitchMode();
+
+    expect(document.getElementById("radio-working-mode").checked).toBe(true);
+  });
+});
+
+describe("runCounter testing", () => {
+    test("should increment counts", () => {
+
+        currMode = "w";
+        counts = 2;
+
+        runCounter();
+
+        expect(counts).toBe(2);
+    })
+})
